09b5225fddf06bf078e86d847b7fbba9263ef47d
Farzad khan <khanfarzd559@gmail.com>
2026-01-14 15:00:03 +0500

    toggle Functionality

---
+++ assets/JS/common.js
@@ -0,0 +1,57 @@
+class commonFunctionality {
+    constructor() {
+        this.#init();
+    }
+
+    #toggle() {
+        const btn = document.getElementById('toggle');
+        const bars = document.querySelectorAll('.bar');
+        const toggleContent = document.querySelector('.toggle-content');
+        const subDropdownIcon = document.querySelector('.dropdown-icon-small');
+        const subdropdownContent = document.querySelector('.subdropdown-content');
+
+        const toggling = eventOn => {
+            eventOn.classList.toggle('active');
+        };
+
+        // navbar toggle button click
+        btn.addEventListener('click', function () {
+            bars.forEach(bar => {
+                toggling(bar);
+            });
+            toggling(toggleContent);
+        });
+
+        // sub toggle contect of services button click
+        subDropdownIcon.addEventListener('click', () => {
+            toggling(subDropdownIcon);
+            toggling(subdropdownContent);
+        });
+    }
+
+    #navbar() {
+        const space = document.querySelector('.spacing');
+        const navbar = document.querySelector('.navbar-fixing');
+
+        window.addEventListener('scroll', () => {
+
+            if (window.scrollY > 30) {
+                space.classList.add('active');
+                navbar.classList.add('active');
+            } else {
+                space.classList.remove('active');
+                navbar.classList.remove('active');
+            }
+
+        });
+
+    }
+
+    #init() {
+        window.addEventListener('DOMContentLoaded', () => {
+            this.#toggle();
+            this.#navbar();
+        });
+    }
+}
+const features = new commonFunctionality();
